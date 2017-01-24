9ba483f8cf1d0d84b6d4bdc1a8b50eafe9191e4d
Rikkokiri <pilvi.rajala@gmail.com>
2017-01-24 13:10:51 +0200

    Added weeks to the countdown

--- js/countdown.js
+++ js/countdown.js
@@ -3,8 +3,8 @@ function calculateRemainingTime(enddate){
     // Difference between the two days in milliseconds
     var timeLeft = Date.parse(enddate) - Date.parse(new Date());
 
-    //var weeks = Math.floor( (timeLeft) / (1000 * 60 * 60 * 24 * 7));
-    var days = Math.floor( (timeLeft / (1000 * 60 * 60 * 24))); //milliseconds * seconds * minutes * hours
+    var weeks = Math.floor( (timeLeft) / (1000 * 60 * 60 * 24 * 7));
+    var days = Math.floor( (timeLeft / (1000 * 60 * 60 * 24)) % 7); //milliseconds * seconds * minutes * hours
     var hours = Math.floor( (timeLeft / (1000 * 60 * 60)) % 24);
     var minutes = Math.floor( (timeLeft / (1000 * 60)) % 60);
     var seconds = Math.floor( (timeLeft / 1000) % 60 );
@@ -12,7 +12,7 @@ function calculateRemainingTime(enddate){
     //Return the values as an object
     return {
       'timeLeft' : timeLeft,
-      //'weeks' : weeks,
+      'weeks' : weeks,
       'days' : days,
       'hours' : hours,
       'minutes' : minutes,
@@ -21,11 +21,12 @@ function calculateRemainingTime(enddate){
 }
 
 
-function startCountdown(endtime, countdownID, daysID, hoursID, minutesID, secondsID) {
+function startCountdown(endtime, countdownID, weeksID, daysID, hoursID, minutesID, secondsID) {
 
     //Get the element where the countdown is located
     var countdownElement = document.getElementById(countdownID);
 
+    var weekSpan = document.getElementById(weeksID);
     var daySpan = document.getElementById(daysID);
     var hourSpan = document.getElementById(hoursID);
     var minuteSpan = document.getElementById(minutesID);
@@ -35,7 +36,7 @@ function startCountdown(endtime, countdownID, daysID, hoursID, minutesID, second
 
       var timeInformation = calculateRemainingTime(endtime);
 
-      //weekSpan.innerHTML = timeInformation.weeks;
+      weekSpan.innerHTML = timeInformation.weeks;
       daySpan.innerHTML = timeInformation.days;
       hourSpan.innerHTML = ('0' + timeInformation.hours).slice(-2);
       minuteSpan.innerHTML = ('0' + timeInformation.minutes).slice(-2);
